662eb0dd036d38723558335f2a53a5d8b75004c7
Orta Therox <orta.therox@gmail.com>
2012-11-06 11:16:20 +0700

    Added another example application

---
+++ Examples/SelectionDelegateExample/SelectionDelegateExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SelectionDelegateExample
+//
+//  Created by orta therox on 06/11/2012.
+//  Copyright (c) 2012 orta therox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <PSTCollectionViewDataSource, PSTCollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate>
+
+@end
