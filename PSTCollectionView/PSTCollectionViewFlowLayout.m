1aa538360f3b4476b8018de2de85683168033a68
Matej Bukovinski <matej@bukovinski.com>
2012-11-28 16:14:59 +0100

    Flow layout contentSize computation improvements.
    - fixes premature cell removal when alwaysBounce* is enabled #117

--- PSTCollectionView/PSTCollectionViewFlowLayout.m
+++ PSTCollectionView/PSTCollectionViewFlowLayout.m
@@ -373,6 +373,11 @@ static char kPSTCachedItemRectsKey;
         }
         section.frame = sectionFrame;
     }
+	if (_data.horizontal) {
+		contentSize.width = MAX(contentSize.width, self.collectionView.bounds.size.width);
+	} else {
+		contentSize.height = MAX(contentSize.height, self.collectionView.bounds.size.height);
+	}
     _data.contentSize = contentSize;
 }
 
