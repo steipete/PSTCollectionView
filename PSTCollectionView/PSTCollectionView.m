f0d97fba07462772f3d2f8180bad2aefb08fa25b
Paul Hecker <paul@iwascoding.com>
2012-12-13 13:21:11 +0100

    Fix for insertSections:
    A call to -[PSTCollectionView insertSections:] has no balanced calls of suspendReloads/resumeReloads and therefore the -[PSTCollectionView reloadData] call has no effect afterwards

--- PSTCollectionView/PSTCollectionView.m
+++ PSTCollectionView/PSTCollectionView.m
@@ -1248,7 +1248,8 @@ static void PSTCollectionViewCommonSetup(PSTCollectionView *_self) {
 }
 
 - (void)resumeReloads {
-    _reloadingSuspendedCount--;
+    if (0 < _reloadingSuspendedCount)
+        _reloadingSuspendedCount--;
 }
 
 -(NSMutableArray *)arrayForUpdateAction:(PSTCollectionUpdateAction)updateAction {
