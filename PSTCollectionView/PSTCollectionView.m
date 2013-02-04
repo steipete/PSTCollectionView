635a8ef2865d35fee88fdfcc8d954e0721323321
Scott Talbot <s@chikachow.org>
2013-02-04 20:38:39 +1100

    Fix compilation without UIKit in a prefix header

--- PSTCollectionView/PSTCollectionView.m
+++ PSTCollectionView/PSTCollectionView.m
@@ -1988,20 +1988,6 @@ static void PSTCollectionViewCommonSetup(PSTCollectionView *_self) {
 
 @end
 
-#if __IPHONE_OS_VERSION_MIN_REQUIRED < 60000
-@implementation NSIndexPath (PSTCollectionViewAdditions)
-
-// Simple NSIndexPath addition to allow using "item" instead of "row".
-+ (NSIndexPath *)indexPathForItem:(NSInteger)item inSection:(NSInteger)section {
-    return [NSIndexPath indexPathForRow:item inSection:section];
-}
-
-- (NSInteger)item {
-    return self.row;
-}
-@end
-#endif
-
 ///////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - Runtime Additions to create UICollectionView
 
