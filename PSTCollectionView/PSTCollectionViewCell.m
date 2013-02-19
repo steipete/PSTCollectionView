fae67a5a35ec69102f932d30948925bf441f5e15
Alexandru Tudose <alextud@gmail.com>
2013-02-19 13:33:15 +0200

    Also update selected background image.

--- PSTCollectionView/PSTCollectionViewCell.m
+++ PSTCollectionView/PSTCollectionViewCell.m
@@ -159,8 +159,10 @@
 // Cell highlighting only highlights the cell itself
 - (void)setHighlighted:(BOOL)highlighted {
     _collectionCellFlags.highlighted = highlighted;
-    
-    [self setHighlighted:highlighted|_collectionCellFlags.selected forViews:self.contentView.subviews];
+
+    BOOL highlightSubviews = highlighted|_collectionCellFlags.selected;
+    _selectedBackgroundView.alpha = highlightSubviews ? 1.0f : 0.0f;
+    [self setHighlighted:highlightSubviews forViews:self.contentView.subviews];
 }
 
 - (void)setHighlighted:(BOOL)highlighted forViews:(id)subviews {
